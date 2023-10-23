c763d5d03a16ad46fe1138fa6208acce89823c14
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2023-10-23 23:40:39 +0300

    feat: add text block and footer, fix slider in cards block

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-const cardsLine = document.querySelector(".popular-slider__cards_line");
+const sliderLine = document.querySelector(".popular-slider__line");
 const arrowPrev = document.querySelector(".arrow-prev");
 const arrowNext = document.querySelector(".arrow-next");
-
-// Slider in About block
 let offset = 0;
-const slideWidth = 329; // Ширина слайда
 
-// Switch slider
-function switchSlide(newOffset) {
-  offset = newOffset;
-  cardsLine.style.left = `${-offset * slideWidth}px`;
-}
-
-// Slider arrows for Prev
 arrowPrev.addEventListener("click", () => {
-  if (offset > 0) {
-    switchSlide(offset - 1);
-  } else {
-    // Если достигнут первый слайд, переместиться в конец
-    switchSlide(cardsLine.childElementCount - 1);
+  offset -= 289 + 40;
+  if (offset < 0) {
+    offset = 0;
   }
+  sliderLine.style.left = `${-offset}px`;
 });
 
-// Slider arrows for Next
 arrowNext.addEventListener("click", () => {
-  if (offset < cardsLine.childElementCount - 1) {
-    switchSlide(offset + 1);
-  } else {
-    // Если достигнут последний слайд, переместиться в начало
-    switchSlide(0);
+  offset += 289 + 40;
+  if (offset > 1200) {
+    offset = 0;
   }
+  sliderLine.style.left = `${-offset}px`;
 });
+
+/* сделать слайдер конечным в одну и в другую сторону и кнопки должны дизактивироваться при окончании слайдера */
+/* или сделать слайдер бесконечным */
